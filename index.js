d81af80d081e1d835d8777a1d38c91dc2ebe9d69
unknown <maxineking@uchicago.edu>
2018-07-24 09:10:27 -0400

    removed comments

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var main = document.getElementById("colors");
 var width = window.innerWidth;
 
 var text = prompt("input some text!");
-//text = "wehgioewhvkzxnvois"
 var numStripes = parseInt(text.length / 3);
 console.log("There are " + numStripes + "stripes");
 
@@ -23,8 +22,6 @@ function makeTextDivs(text, stripe){
         var tdiv = document.createElement("div");
         tdiv.className = "textDiv";
         tdiv.innerHTML = text[i];
-//        tdiv.style.height = (height*0.2) + "px";
-//        tdiv.style.fontSize = (height*0.2) + "px";
         tdiv.style.height = textSize;
         tdiv.style.fontSize = textSize;
         var theight = tdiv.style.height;
@@ -39,8 +36,6 @@ function makeStripe(stripeText){
 
     var stripe = document.createElement("div");
     stripe.className = "stripe";
-//    stripe.style.width = (100/numStripes) + "%"
-//    var id = "stripe" + i
 
 
     var r = stripeText.charCodeAt(0);
@@ -51,7 +46,6 @@ function makeStripe(stripeText){
 
     stripe.style.width = (100/numStripes) + "%";
     stripe.style.backgroundColor = rgb;
-//    stripe.style.color = opposite(r,g,b);
     stripe.style.color = ((r+g+b) < 200) ? "white" : "black";
     stripe.style.textAlign = "center";
         
@@ -64,17 +58,3 @@ for (var i = 0; i < numStripes; i++){
     text = text.slice(3);
     makeStripe(stripeText);
 }
-/*
-while (numchar != 3){
-    word = prompt("type a three-letter word")
-    numchar = word.length;
-}
-
-
-console.log(r);
-console.log(g);
-console.log(b);
-
-
-document.getElementById("colors").style.backgroundColor = rgb;
-*/
