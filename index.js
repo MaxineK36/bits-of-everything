70be34aa068a4522d9c0fffd302251d273549bfd
Maxine King <maxineking@cs.uchicago.edu>
2018-07-25 21:54:44 -0400

    added an option to use two characters per r, g, b, to make for brighter colors

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var numchar = 0;
 var main = document.getElementById("colors");
 var width = window.innerWidth;
 
-var text = prompt("input some text!");
 //var text ="no!";
-var numStripes = parseInt(text.length / 3);
+var text;
+var numStripes;
 
 function opposite(r, g, b){
     var or = 255 - r
@@ -44,6 +44,24 @@ function makeTextDivs(text, stripe){
     } 
     stripe.appendChild(threeChar);
 }
+
+function makeTwoCharTextDivs(text, stripe){
+    var sixChar = document.createElement("div");
+    sixChar.style.height = "50%";
+    sixChar.style.width = "100%";
+    var textSize = (innerWidth / numStripes)/6 ;
+    for (var i = 0; i<text.length; i+=2){
+        var tdiv = document.createElement("div");
+        tdiv.className = "textDiv";
+        tdiv.innerHTML = text[i] + text[i+1];
+        tdiv.style.height = textSize;
+        tdiv.style.fontSize = textSize;
+        var theight = tdiv.style.height;
+        sixChar.appendChild(tdiv);
+    } 
+    stripe.appendChild(sixChar);
+}
+
 
 function makeColorDivs(stripe, colors){
     //var bases = ["(255,0,0)","(0,255,0)","(0,0,255)"];
@@ -86,12 +104,49 @@ function makeStripe(stripeText){
     makeColorDivs(stripe,[r,g,b]);
 }
 
-for (var i = 0; i < numStripes; i++){
-    var stripeText = text.slice(0,3); 
-    text = text.slice(3);
-    makeStripe(stripeText);
+function makeBrighterStripe(stripeText){
+    console.log("making a brighter stripe");
+    var stripe = document.createElement("div");
+    stripe.className = "stripe";
+
+
+    var r = stripeText.charCodeAt(0) + stripeText.charCodeAt(1);
+    var g = stripeText.charCodeAt(2) + stripeText.charCodeAt(3);
+    var b = stripeText.charCodeAt(4) + stripeText.charCodeAt(5);
+    var rgb = "rgb(" + r + "," + g + "," + b + ")";
+
+    stripe.style.width = (100/numStripes) + "%";
+    stripe.style.backgroundColor = rgb;
+    stripe.style.color = ((r+g+b) < 200) ? "white" : "black";
+    stripe.style.textAlign = "center";
+    main.appendChild(stripe);
+    makeTwoCharTextDivs(stripeText, stripe);
+    makeColorDivs(stripe,[r,g,b]);
+
 }
 
-for (var i=0; i<21; i++){
-    console.log(intToBinary(i));
+var oneortwo = prompt("Enter 1 to use one character per color, or 2 to use two");
+console.log(oneortwo);
+text = prompt("input some text!");
+
+if (oneortwo == "1"){
+    numStripes = parseInt(text.length / 3);
+    for (var i = 0; i < numStripes; i++){
+        var stripeText = text.slice(0,3); 
+        text = text.slice(3);
+        makeStripe(stripeText);
+    }
+} else if (oneortwo == "2"){
+    numStripes = parseInt(text.length / 6); 
+    for (var i = 0; i < numStripes; i++){
+        var stripeText = text.slice(0,6); 
+        text = text.slice(6);
+        makeBrighterStripe(stripeText);
+    }
+} else {
+    alert("Invalid input. Reload to try again");
 }
+
+
+
+
