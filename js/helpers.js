bdf5f62c3aaa801876e48abe09b26a7c39401b75
unknown <maxineking@uchicago.edu>
2018-08-17 16:28:02 -0400

    working commit

--- js/helpers.js
+++ js/helpers.js
@@ -88,3 +88,8 @@ function intToBinary(number, minLength){
     }   
     return binText;
 }
+
+function hexToInt(hexNumber){
+    var hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    return anyBaseToInt(hex, 16, hexNumber);
+}
